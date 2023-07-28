0639982f66f61398ea2537b3ce6cb655ad28fe8c
NavyBlueSky <lylebang9397@gmail.com>
2023-07-28 19:56:50 +0900

    230728 nodejs 실습

--- 실습/4주차5차시/index.js
+++ 실습/4주차5차시/index.js
@@ -10,13 +10,27 @@ app.set('views', './views');
 app.use('/public', express.static('./public'));
 
 app.get('/', (req, res) => {
-    res.render('prac3', {
-        number: [1,2,3,4,5,6,7,8,9]
+    res.render('prac4', {
+
     });
 });
 
-app.get('/kdt9', (req, res) => {
+app.get('/pracdomain1', (req, res) => {
     res.render('prac2', {name: 'lyle'}) 
+})
+
+app.get('/pracdomain2', (req, res) => {
+    res.render('prac1', {name2: 'Hello there'})
+})
+
+app.get('/pracdomain3', (req, res) => {
+    res.render('prac3', {
+        number: [1,2,3,4,5,6,7,8,9]
+    })
+})
+
+app.get('/pracdomain4', (req, res) => {
+    res.render('pracimg', {})
 })
 
 app.listen(PORT, () => {
