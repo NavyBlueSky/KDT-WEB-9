9c35284b22f24da7f2b3d26f984c9b5889a81670
NavyBlueSky <lylebang9397@gmail.com>
2023-08-05 18:32:01 +0900

    230805 nodejs 자습

--- 실습/5주차4차시/index.js
+++ 실습/5주차4차시/index.js
@@ -4,62 +4,42 @@ const path = require('path');
 const app = express();
 const PORT = 8000;
 
-//view engine
-app.set('view engine', 'ejs');
-app.set('views', './views');
-
 //body-parser
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// 정적파일 설정
+//view engine
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+//정적파일
 app.use('/uploads', express.static(__dirname + '/uploads'));
 
-
-
-
-
-// //multer => body-parser 바로뒤에 줌
-// const upload = multer({
-//     //dest - 업로드할 파일이 저장되는 경로를 지정
-//     dest: 'uploads/'
-// });
-// const uploadDetail = multer({
-//     //storage: 저장할 공간에 대한 정보
-//     //diskStorage: 파일을 디스크에 저장하기 위한 모든 제어 기능을 제공
-//     storage: multer.diskStorage({
-//         //done : 그냥 변수명 , null: 에러처리하기 위함
-//         destination(req, file, done) {
-//             done(null, 'uploads/')
-//         },
-//         filename(req, file, done) {
-//             const ext = path.extname(file.originalname);
-//             console.log('ext', ext);
-//             done(null, file.fieldname + ext);
-//         },
-//     }),
-//     limits: {fileSize : 5 * 1024 * 1024 },
-// });
+//multer세팅
+const uploadDetail = multer({
+    storage: multer.diskStorage({
+        destination(req, file, done) {
+            done(null, 'uploads/');
+        },
+        filename(req, file, done) {
+            console.log('filename: ', req.body);
+            const ext = path.extname(file.originalname);
+            done(null, req.body.userId + Date.now() + ext);
+        },
+    }),
+    limits: { fileSize: 10 * 1024 * 1024 },
+});
 
 //router
 app.get('/', (req, res) => {
     res.render('index');
 });
-app.post('/result', (req, res) => {
-    res.render('result');
-})
-
-// // 싱글
-// app.post('/upload', uploadDetail.single('userfile'), (req, res) => {
-//     console.log(req.file);
-//     console.log(req.body);
-// });
-
-// // 동적
-// app.post('/dynamicFile', uploadDetail.single('dynamic-file'), (req, res) => {
-//     console.log(req.file);
-//     res.send(req.file);
-// });
+app.post('/result1', uploadDetail.single('profile'), (req, res) => {
+    res.render('result', {
+        userInfo: req.body,
+        profile: req.file.path,
+    });
+});
 
 //server
 app.listen(PORT, () => {
