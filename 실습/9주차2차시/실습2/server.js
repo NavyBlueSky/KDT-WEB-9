ea5083091044c521430c31a76bb18b19b2b34900
NavyBlueSky <lylebang9397@gmail.com>
2023-08-30 10:26:00 +0900

    230830 ung's code

--- 실습/9주차2차시/실습2/server.js
+++ 실습/9주차2차시/실습2/server.js
@@ -1,13 +1,11 @@
-const http = require('http');
-const express = require('express');
-const SocketIO = require('socket.io');
+const http = require("http");
+const express = require("express");
+const SocketIO = require("socket.io");
 
 const app = express();
 const PORT = 8000;
 
-//http 서버
 const server = http.createServer(app);
-//socket서버
 const io = SocketIO(server);
 
 app.set('view engine', 'ejs');
@@ -15,50 +13,37 @@ app.set('view engine', 'ejs');
 app.get('/', (req, res) => {
     res.render('chat');
 });
-// app.get('/chat', (req, res) => {
-//     res.render('chat');
-// });
 
 io.on('connection', (socket) => {
-    console.log('조인 전', socket.rooms); // 새로고침할 때마다 소켓의 아이디값이 바뀐다.
-    socket.on('join', (res) => {
-        //채팅방을 생성하는 방법은 join(방아이디) 사용. 방이 존재하면 그 방으로 접속
-        socket.join(res);
-        socket.room = res;
-        console.log('조인 후', socket.rooms);
-        //broadcast는 나를 제외한 전체 사용자(브라우저)에게 메세지를 전달
-        socket.broadcast.to(res).emit('create', '새로운 브라우저가 입장했습니다.');
-        //console.log(socket);
-        const roomInfo = io.sockets.adapter.rooms.get(res)?.size;
-        //console.log(roomInfo);
+
+    // 소켓 아이디값 나옴
+    console.log('조인 전', socket.rooms);
+    socket.on('join', (room) => {
+       // 채팅방 생성 -> join() 사용 
+       // 조인은 방을 생성하면서 그 방이 존재하면 거기로 접속
+      socket.join(room); 
+      socket.room = room;
+      console.log('조인 후', socket.rooms);
+      // 나를 제외한 전체사용자에게 메세지 전달
+      socket.broadcast.to(room).emit('create', `${[...socket.rooms][0]}님이 입장하였습니다`);
+      // set 길이 -> size
+      const roomInfo = io.sockets.adapter.rooms.get(room);
+      //console.log(io.sockets.adapter);
+      console.log(roomInfo?.size);
     });
-    socket.on('message', (res) => {
-        //io.to(특정방아이디).emit(이벤트) 특정방의 전체 사용자에게 메세지 전달
-        //io.to(socket.room).emit('chat', res);
-        //socket.emit('chat', res)
-        socket.broadcast.to(socket.room).emit('oppo', res);
+
+    socket.on('sendMessage', (message) => {
+        // io.to(특정방 id).emit(이벤트) -> 특정방의 전체 사용자들에게 메세지 전달
+        // io.to(socket.room).emit('chat', message);
+        socket.broadcast.to(socket.room).emit('chat', message);
     });
+
     socket.on('leave', () => {
         socket.leave(socket.room);
-        const roomInfo = io.sockets.adapter.rooms.get(socket.room)?.size;
-        console.log(roomInfo);
+        console.log(`남은인원: ${io.sockets.adapter.rooms.get(socket.room)?.size}`);
     });
+    
 });
-
-
-// io.on('connection', (socket) => {
-//     socket.on('open_message', (arg, cb) => {
-//         console.log(arg); //, arg2, arg3, arg4);
-//         cb(arg);
-//     });
-
-//     socket.on('form_message', (arg) => {
-//         console.log(arg);
-//         socket.emit('backend_message', arg);
-//     });
-// });
-
-
 
 server.listen(PORT, () => {
     console.log(`http://localhost:${PORT}`);
