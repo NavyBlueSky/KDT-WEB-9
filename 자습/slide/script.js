4b4e0180a6292258abb3f4925e12287944ac973c
NavyBlueSky <lylebang9397@gmail.com>
2023-09-13 06:00:59 +0900

    230913 chat

--- 자습/slide/script.js
+++ 자습/slide/script.js
@@ -7,13 +7,13 @@ const wrapper = document.querySelector(".wrapper"),
 let imageIndex = 1,
   intervalId;
 
-// // Define function to start automatic image slider
-// const autoSlide = () => {
-//   // Start the slideshow by calling slideImage() every 2 seconds
-//   intervalId = setInterval(() => slideImage(++imageIndex), 2000);
-// };
-// // Call autoSlide function on page load
-// autoSlide();
+// Define function to start automatic image slider
+const autoSlide = () => {
+  // Start the slideshow by calling slideImage() every 2 seconds
+  intervalId = setInterval(() => slideImage(++imageIndex), 2000);
+};
+// Call autoSlide function on page load
+autoSlide();
 
 // A function that updates the carousel display to show the specified image
 const slideImage = () => {
