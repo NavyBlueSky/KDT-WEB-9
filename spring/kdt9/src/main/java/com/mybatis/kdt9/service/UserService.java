8b6477eed579d736a08d3fb22d7a20ec8cb09acf
NavyBlueSky <lylebang9397@gmail.com>
2023-11-07 19:08:26 +0900

    231107 spring builder

--- spring/kdt9/src/main/java/com/mybatis/kdt9/service/UserService.java
+++ spring/kdt9/src/main/java/com/mybatis/kdt9/service/UserService.java
@@ -2,7 +2,10 @@ package com.mybatis.kdt9.service;
 
 import com.mybatis.kdt9.domain.User;
 import com.mybatis.kdt9.dto.UserDTO;
+import com.mybatis.kdt9.dto.UserDTOBuilder;
 import com.mybatis.kdt9.mapper.UserMapper;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -22,19 +25,34 @@ public class UserService {
 
         for( int i = 0; i < result.size(); i++) {
             User u = result.get(i);
-            UserDTO user = new UserDTO();
-            user.setId(u.getId());
-            user.setName(u.getName());
-            user.setNickname(u.getNickname());
-            user.setNo(i + 1);
+
+//            UserDTO user = new UserDTO();
+//            user.setId(u.getId());
+//            user.setName(u.getName());
+//            user.setNickname(u.getNickname());
+//            user.setNo(i + 1);
+//            users.add(user);
+            UserDTO user = UserDTO.builder()
+                    .id(u.getId())
+                    .name(u.getName())
+                    .nickname(u.getNickname())
+                    .no(i+1)
+                    .build();
             users.add(user);
+
 
             // 검색된 u 하나씩 데이터를 userDTO로 만들고
             // 그걸 users에 담아서 return 해야겟죠?
+
+//            UserDTOBuilder userDTOBuilder = UserDTOBuilder.builder()
+//                    .id(u.getId())
+//                    .name(u.getName())
+//                    .build();
         }
 
         return users;
     }
+
 
     public void insertUser(String name, String nickname) {
         User user = new User();
