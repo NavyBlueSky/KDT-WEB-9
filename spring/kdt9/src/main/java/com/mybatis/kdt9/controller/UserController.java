52c24f85e29a19378bd3cce928ad759fe563974c
NavyBlueSky <lylebang9397@gmail.com>
2023-11-09 23:20:19 +0900

    231109 spring jpa crud

--- spring/kdt9/src/main/java/com/mybatis/kdt9/controller/UserController.java
+++ spring/kdt9/src/main/java/com/mybatis/kdt9/controller/UserController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
@@ -38,5 +39,17 @@ public class UserController {
 
         return "user";
     }
+
+    @GetMapping("/search")
+    public String selectUser(@RequestParam String name){
+        return userService.searchUser(name);
+    }
+
+    @GetMapping("/searchId")
+    @ResponseBody
+    public String searchId(@RequestParam int id){
+        return userService.searchId(id);
+    }
+
 
 }
--- spring/kdt9/src/main/java/com/mybatis/kdt9/service/UserService.java
+++ spring/kdt9/src/main/java/com/mybatis/kdt9/service/UserService.java
@@ -1,16 +1,20 @@
 package com.mybatis.kdt9.service;
 
 import com.mybatis.kdt9.domain.User;
+import com.mybatis.kdt9.domain.UserEntity;
 import com.mybatis.kdt9.dto.UserDTO;
 import com.mybatis.kdt9.dto.UserDTOBuilder;
 import com.mybatis.kdt9.mapper.UserMapper;
+import com.mybatis.kdt9.repository.UserRepository;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.swing.text.html.Option;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -19,36 +23,84 @@ public class UserService {
     @Autowired
     UserMapper userMapper;
 
+    @Autowired
+    UserRepository userRepository;
+
+    public String searchId(int id) {
+        // Optional: null일수도 있는 객체를 감싸는 wrapper 객체
+        Optional<UserEntity> result = userRepository.findById(id);
+        if(result.isPresent()){
+            System.out.println(result.get().getName());
+        } else {
+            System.out.println("해당하는 친구 없음");
+        }
+        // optional 함수
+        // isPresent(), isEmpty(), get() 등등
+        // isPresent(): 객체의 여부를 boolean으로 반환
+        // isEmpty(): isPresent()의 반대
+        // get(): 옵셔널 내부의 객체를 반환
+
+        return "";
+    }
+
+
+    public String searchUser(String name) {
+        // repository 를 통해서 어떤 메소드를 실행하고 그에 대한 결과를 return
+        List<UserEntity> result = userRepository.findByName(name);
+        // findBy 컬럼명
+        // findBy 컬럼명 and 컬럼명 findByNameAndNickName
+        for (UserEntity user : result) {
+            System.out.println(user.getId() + user.getNickname());
+        }
+        return "";
+    }
+
+
+
     public List<UserDTO> getUserList(){
-        List<User> result = userMapper.retrieveAll();
-        List<UserDTO> users = new ArrayList<UserDTO>();
-
-        for( int i = 0; i < result.size(); i++) {
-            User u = result.get(i);
-
-//            UserDTO user = new UserDTO();
-//            user.setId(u.getId());
-//            user.setName(u.getName());
-//            user.setNickname(u.getNickname());
-//            user.setNo(i + 1);
-//            users.add(user);
-            UserDTO user = UserDTO.builder()
-                    .id(u.getId())
-                    .name(u.getName())
-                    .nickname(u.getNickname())
-                    .no(i+1)
+        ////////////////////jpa////////////////////
+        List<UserEntity> result = userRepository.findAll(); // 전체 검색 select * from user;
+        List<UserDTO> users = new ArrayList<>();
+        for (UserEntity user : result) {
+            UserDTO userDTO = UserDTO.builder()
+                    .id(user.getId())
+                    .name(user.getName())
+                    .nickname(user.getNickname())
                     .build();
-            users.add(user);
+            users.add(userDTO);
+        }
 
 
-            // 검색된 u 하나씩 데이터를 userDTO로 만들고
-            // 그걸 users에 담아서 return 해야겟죠?
-
-//            UserDTOBuilder userDTOBuilder = UserDTOBuilder.builder()
+        //////////////////////////////mybatis///////////////////////////////
+//        List<User> result = userMapper.retrieveAll();
+//        List<UserDTO> users = new ArrayList<UserDTO>();
+//
+//        for( int i = 0; i < result.size(); i++) {
+//            User u = result.get(i);
+//
+////            UserDTO user = new UserDTO();
+////            user.setId(u.getId());
+////            user.setName(u.getName());
+////            user.setNickname(u.getNickname());
+////            user.setNo(i + 1);
+////            users.add(user);
+//            UserDTO user = UserDTO.builder()
 //                    .id(u.getId())
 //                    .name(u.getName())
+//                    .nickname(u.getNickname())
+//                    .no(i+1)
 //                    .build();
-        }
+//            users.add(user);
+//
+//
+//            // 검색된 u 하나씩 데이터를 userDTO로 만들고
+//            // 그걸 users에 담아서 return 해야겟죠?
+//
+////            UserDTOBuilder userDTOBuilder = UserDTOBuilder.builder()
+////                    .id(u.getId())
+////                    .name(u.getName())
+////                    .build();
+//        }
 
         return users;
     }
@@ -60,5 +112,13 @@ public class UserService {
         user.setNickname(nickname);
 
         userMapper.insertUser(user);
+    }
+
+    public void insertUserE(String name, String nickname) {
+        UserEntity userEntity = new UserEntity();
+        userEntity.setName(name);
+        userEntity.setNickname(nickname);
+
+        userRepository.insertUserE(userEntity);
     }
 }
